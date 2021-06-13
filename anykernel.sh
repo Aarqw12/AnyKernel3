d95790aca0d2231cf7348a3fb7104977762c79f9
Aarqw12 <lcockx@protonmail.com>
2021-06-13 07:52:56 -0400

    anykernel: update kernel description

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=-- KERNEL REBORN PREMIUM OSS --
+kernel.string= KERNEL REBORN 11 --open-source project --
 do.devicecheck=1
 do.modules=0
 do.systemless=1
--- pack.sh
+++ pack.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -x
 rm *.zip
-zip -r9 reborn-premium-$(date +%Y%m%d).zip * -x .git README.md *placeholder
+zip -r9 reborn-$(date +%Y%m%d).zip * -x .git README.md *placeholder
